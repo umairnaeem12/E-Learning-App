fac6df92b6c52ab2d47fb2af38504014df76ed6d
umairnaeem12 <124865412+umairnaeem12@users.noreply.github.com>
2023-06-23 10:27:51 +0500

    Update FirstScreen.js

--- src/OnBoarding/FirstScreen.js
+++ src/OnBoarding/FirstScreen.js
@@ -19,7 +19,7 @@ const FirstScreen = () => {
                 // img={require('../Images/vr.jpg')}
                 img={{uri: 'https://static.vecteezy.com/system/resources/previews/001/811/016/non_2x/online-education-e-learning-online-course-concept-home-school-illustration-students-on-laptop-computer-screen-distance-learning-new-normal-cartoon-flat-illustration-vector.jpg'}}
                 title={`Explore Online ${'\n'}Courses`}
-                text={'ꜰɪɴᴅ ʙᴇꜱᴛ ᴄᴏᴜʀꜱᴇ ꜰᴏʀ ʏᴏᴜʀ ᴄᴀʀᴇᴇʀ ᴛʜᴀᴛ ᴡɪʟʟ ʜᴇʟᴘ ʏᴏᴜ ᴛᴏ ᴅᴇᴠᴇʟᴏᴘ ʏᴏᴜʀ ꜱᴋɪʟʟsssssss.'}
+                text={'ꜰɪɴᴅ ʙᴇꜱᴛ ᴄᴏᴜʀꜱᴇ ꜰᴏʀ ʏᴏᴜʀ ᴄᴀʀᴇᴇʀ ᴛʜᴀᴛ ᴡɪʟʟ ʜᴇʟᴘ ʏᴏᴜ ᴛᴏ ᴅᴇᴠᴇʟᴏᴘ ʏᴏᴜʀ ꜱᴋɪʟʟ.'}
             />
 
             <BottomDotBtnCom onPress={handlePress} />
